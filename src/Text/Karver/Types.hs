ff09cfd4e529251d81c44fa8e622f7289654d92a
Jeremy Hull <sourdrums@gmail.com>
2013-05-24 15:31:47 -0700

    Add Object type to Token optional types

--- src/Text/Karver/Types.hs
+++ src/Text/Karver/Types.hs
@@ -2,6 +2,9 @@ module Text.Karver.Types where
 
 import Data.Text (Text)
 
+type Key = Text
+
 data Tokens = Literal  Text
             | Identity Text
+            | Object   Text Key
             deriving (Show, Eq)

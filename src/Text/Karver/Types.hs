ad12d1d695e33d5c9d2e1a62836d4faed00c6b02
Jeremy Hull <sourdrums@gmail.com>
2013-05-24 17:31:33 -0700

    Add Value type to accommodate object identities

--- src/Text/Karver/Types.hs
+++ src/Text/Karver/Types.hs
@@ -1,6 +1,7 @@
 module Text.Karver.Types where
 
 import Data.Text (Text)
+import Data.HashMap.Strict
 
 type Key = Text
 
@@ -8,3 +9,7 @@ data Tokens = Literal  Text
             | Identity Text
             | Object   Text Key
             deriving (Show, Eq)
+
+data Value = String Text
+           | Map    (HashMap Text Text)
+           deriving (Show)

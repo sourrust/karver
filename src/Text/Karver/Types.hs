286b9fba0634ec93994aa31870e87a6e1595ea1a
Jeremy Hull <sourdrums@gmail.com>
2013-05-25 18:11:35 -0700

    Add types for the array representation

--- src/Text/Karver/Types.hs
+++ src/Text/Karver/Types.hs
@@ -2,14 +2,18 @@ module Text.Karver.Types where
 
 import Data.Text (Text)
 import Data.HashMap.Strict
+import Data.Word (Word32)
+import Data.Vector
 
 type Key = Text
 
 data Tokens = Literal  Text
             | Identity Text
             | Object   Text Key
+            | Array    Text Word32
             deriving (Show, Eq)
 
 data Value = String Text
            | Map    (HashMap Text Text)
+           | List   (Vector Text)
            deriving (Show)

03ead5328dabca80148403a44edcdf3ff9e5cab3
Jeremy Hull <sourdrums@gmail.com>
2013-05-25 19:26:58 -0700

    Change array index to Int
    This is just so I don't have to convert Word32 to Int for functions like !
    when accessing vector elements.

--- src/Text/Karver/Types.hs
+++ src/Text/Karver/Types.hs
@@ -2,7 +2,6 @@ module Text.Karver.Types where
 
 import Data.Text (Text)
 import Data.HashMap.Strict
-import Data.Word (Word32)
 import Data.Vector
 
 type Key = Text
@@ -10,7 +9,7 @@ type Key = Text
 data Tokens = Literal  Text
             | Identity Text
             | Object   Text Key
-            | Array    Text Word32
+            | Array    Text Int
             deriving (Show, Eq)
 
 data Value = String Text

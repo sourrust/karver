141c3e28a3ab287f53b2f6979715b06e03b7f7f5
Jeremy Hull <sourdrums@gmail.com>
2013-05-25 18:12:57 -0700

    Add tests for arrayParser

--- test/Text/Karver/ParseSpec.hs
+++ test/Text/Karver/ParseSpec.hs
@@ -5,16 +5,19 @@ module Text.Karver.ParseSpec (spec) where
 import Text.Karver.Parse
 import Text.Karver.Types
 
+import Prelude hiding (concat)
 import Data.Attoparsec.Text (parseOnly)
-import Data.Text (Text)
+import Data.Text (Text, concat, pack)
 import Test.Hspec
 
-literal, ident, object :: Text -> Either String Tokens
+literal, ident, object, array :: Text -> Either String Tokens
 literal = parseOnly literalParser
 
 ident = parseOnly identityParser
 
 object = parseOnly objectParser
+
+array = parseOnly arrayParser
 
 isLeft :: Either a b -> Bool
 isLeft (Left _) = True
@@ -96,5 +99,30 @@ spec = do
       let regObj   = "{{ person.name }}"
           value    = object regObj
           expected = Right $ Object "person" "name"
+
+      value `shouldBe` expected
+
+  describe "arrayParser" $ do
+    it "no array present" $ do
+      let noArr = "{{ name }}"
+          value = array noArr
+
+      value `shouldSatisfy` isLeft
+
+    it "regular array" $ do
+      let regArr   = "{{ names[1] }}"
+          value    = array regArr
+          expected = Right $ Array "names" 1
+
+      value `shouldBe` expected
+
+    it "maxBound index array" $ do
+      let maxWord  = maxBound
+          regArr   = concat [ "{{ names["
+                            , (pack $ show maxWord)
+                            , "] }}"
+                            ]
+          value    = array regArr
+          expected = Right $ Array "names" maxWord
 
       value `shouldBe` expected
